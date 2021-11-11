d1d37cab8b269f3b52e0af0b94854537b7130cf5
shahithabegum <69446892+shahithabegum@users.noreply.github.com>
2021-11-11 16:29:01 +0530

    Add files via upload

---
+++ se19.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.*;
+class se19
+{
+  public static void main(String arg[])
+   {
+     try
+         {
+            String st="The quick brwon fox jumbs over on the lazy dog ";
+	int a=st.indexOf("a");
+	int b=st.indexOf("b");
+	int c=st.indexOf("c");
+	int d=st.indexOf("d");
+	int e=st.indexOf("e");
+	int f=st.indexOf("f");
+	int i=st.indexOf("i");
+	int j=st.indexOf("j");
+	int k=st.indexOf("k");	
+	int l=st.indexOf("l");
+	int m=st.indexOf("m");
+	int n=st.indexOf("n");
+	int o=st.indexOf("o");
+	int p=st.indexOf("p");
+	int q=st.indexOf("q");
+	int r=st.indexOf("r");
+	int s=st.indexOf("s");
+	int t=st.indexOf("t");
+	int u=st.indexOf("u");
+	int v=st.indexOf("v");
+	int w=st.indexOf("w");	
+	int x=st.indexOf("x");
+	int y=st.indexOf("y");
+	int z=st.indexOf("z");
+                  System.out.println(a+" "+b+" "+c+" "+d+" "+e+" "+f+" "+i+" "+j);
+	System.out.println("================================");
+	System.out.println(k+" "+l+" "+m+" "+n+" "+o+" "+p+" "+q+" "+r);
+	System.out.println("================================");
+	System.out.println(s+" "+t+" "+u+" "+v+" "+w+" "+x+" "+y+" "+z);
+	System.out.println("================================");
+	}
+	catch(Exception e)
+	{
+	  System.out.println(e);
+	}
+       }
+}
